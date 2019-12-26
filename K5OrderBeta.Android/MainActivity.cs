f21c6b63bd02dba0cd0919380b7dde355d4fb92d
Kenneth Lacaron <52525945+KendrewLacaron@users.noreply.github.com>
2019-12-26 19:30:31 +0800

    Add files via upload

---
+++ K5OrderBeta.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace K5OrderBeta.Droid
+{
+    [Activity(Label = "K5OrderBeta", Icon = "@drawable/k5", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Forms.SetFlags("CollectionView_Experimental");
+            Forms.Init(this, savedInstanceState);
+            Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
